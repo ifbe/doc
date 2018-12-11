8dce8f9168724b1d905eec399ee903896d0cfec0
ifbe <ifbe@live.cn>
2018-12-11 13:50:00 +0800

    fix usbgadgetfix usbgadgetfix usbgadget

--- hardware1/usbgadget/usbgadget.sh
+++ hardware1/usbgadget/usbgadget.sh
@@ -22,30 +22,34 @@ echo "42424242" > strings/0x409/serialnumber
 echo "pipipipi" > strings/0x409/manufacturer
 echo "pi0w"   > strings/0x409/product
 
-#configs
+#1 serial
+mkdir -p functions/acm.usb0
+
+#2 network
+mkdir -p functions/rndis.usb0
+
+#3 keyboard
+#mkdir -p functions/hid.usb0
+#echo 1 > functions/hid.usb0/protocol
+#echo 1 > functions/hid.usb0/subclass
+#echo 8 > functions/hid.usb0/report_length
+#echo -ne \\x05\\x01\\x09\\x06\\xa1\\x01\\x05\\x07\\x19\\xe0\\x29\\xe7\\x15\\x00\\x25\\x01\\x75\\x01\\x95\\x08\\x81\\x02\\x95\\x01\\x75\\x08\\x81\\x03\\x95\\x05\\x75\\x01\\x05\\x08\\x19\\x01\\x29\\x05\\x91\\x02\\x95\\x01\\x75\\x03\\x91\\x03\\x95\\x06\\x75\\x08\\x15\\x00\\x25\\x65\\x05\\x07\\x19\\x00\\x29\\x65\\x81\\x00\\xc0 > functions/hid.usb0/report_desc
+
+# put here
 mkdir -p configs/c.1
 echo 250 > configs/c.1/MaxPower
 
-#1 serial
-mkdir -p functions/acm.usb0
+# 3, 2, 1
+#ln -sf functions/hid.usb0   configs/c.1/
+ln -sf functions/rndis.usb0 configs/c.1/
 ln -sf functions/acm.usb0   configs/c.1/
 
-#2 keyboard
-mkdir -p functions/hid.usb0
-echo 1 > functions/hid.usb0/protocol
-echo 1 > functions/hid.usb0/subclass
-echo 8 > functions/hid.usb0/report_length
-echo -ne \\x05\\x01\\x09\\x06\\xa1\\x01\\x05\\x07\\x19\\xe0\\x29\\xe7\\x15\\x00\\x25\\x01\\x75\\x01\\x95\\x08\\x81\\x02\\x95\\x01\\x75\\x08\\x81\\x03\\x95\\x05\\x75\\x01\\x05\\x08\\x19\\x01\\x29\\x05\\x91\\x02\\x95\\x01\\x75\\x03\\x91\\x03\\x95\\x06\\x75\\x08\\x15\\x00\\x25\\x65\\x05\\x07\\x19\\x00\\x29\\x65\\x81\\x00\\xc0 > functions/hid.usb0/report_desc
-ln -sf functions/hid.usb0   configs/c.1/
-
-#3 network
-mkdir -p functions/rndis.usb0
+# os descriptors
 echo 1       > os_desc/use
 echo 0xcd    > os_desc/b_vendor_code
 echo MSFT100 > os_desc/qw_sign
 echo RNDIS   > functions/rndis.usb0/os_desc/interface.rndis/compatible_id
 echo 5162001 > functions/rndis.usb0/os_desc/interface.rndis/sub_compatible_id
-ln -sf functions/rndis.usb0 configs/c.1/
 ln -sf configs/c.1 os_desc
 
 # trigger
