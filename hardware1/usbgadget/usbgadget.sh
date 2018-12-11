f1b128d1909e64765b03eccf432c180a845b7037
ifbe <ifbe@live.cn>
2018-12-11 14:47:13 +0800

    usbgadget: rndis+serial+hid+massusbgadget: rndis+serial+hid+massusbgadget: rndis+serial+hid+mass

--- hardware1/usbgadget/usbgadget.sh
+++ hardware1/usbgadget/usbgadget.sh
@@ -22,6 +22,10 @@ echo "42424242" > strings/0x409/serialnumber
 echo "pipipipi" > strings/0x409/manufacturer
 echo "pi0w"   > strings/0x409/product
 
+# config 1
+mkdir -p configs/c.1
+echo 250 > configs/c.1/MaxPower
+
 #1 network (must be the first for windows)
 mkdir -p functions/rndis.usb0
 
@@ -35,14 +39,19 @@ echo 1 > functions/hid.usb0/subclass
 echo 8 > functions/hid.usb0/report_length
 echo -ne \\x05\\x01\\x09\\x06\\xa1\\x01\\x05\\x07\\x19\\xe0\\x29\\xe7\\x15\\x00\\x25\\x01\\x75\\x01\\x95\\x08\\x81\\x02\\x95\\x01\\x75\\x08\\x81\\x03\\x95\\x05\\x75\\x01\\x05\\x08\\x19\\x01\\x29\\x05\\x91\\x02\\x95\\x01\\x75\\x03\\x91\\x03\\x95\\x06\\x75\\x08\\x15\\x00\\x25\\x65\\x05\\x07\\x19\\x00\\x29\\x65\\x81\\x00\\xc0 > functions/hid.usb0/report_desc
 
-# config 1
-mkdir -p configs/c.1
-echo 250 > configs/c.1/MaxPower
+#4 mass storage
+#mkdir -p functions/mass_storage.usb0
+#echo 1 > functions/mass_storage.usb0/stall
+#echo 1 > functions/mass_storage.usb0/lun.0/removable
+#echo 0 > functions/mass_storage.usb0/lun.0/cdrom
+###echo 0 > functions/mass_storage.usb0/lun.0/nofua
+#echo "/home/pi/usbgadget.img" > functions/mass_storage.usb0/lun.0/file
 
 # 1, 2, 3
-ln -sf functions/rndis.usb0 configs/c.1/
-ln -sf functions/acm.usb0   configs/c.1/
-ln -sf functions/hid.usb0   configs/c.1/
+ln -sf functions/rndis.usb0        configs/c.1/
+ln -sf functions/acm.usb0          configs/c.1/
+ln -sf functions/hid.usb0          configs/c.1/
+#ln -sf functions/mass_storage.usb0 configs/c.1/
 
 # os descriptors
 echo 1       > os_desc/use
